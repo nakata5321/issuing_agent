ab7eec80f3f223fbfa76f607eca5f891cc00fb06
Vadim Manaenko <vadim.razorq@gmail.com>
2019-07-02 15:14:35 +0300

    Delete everything that is not needed here

--- dapp/src/utils/utils.js
+++
@@ -1,8 +0,0 @@
-export const toWei = (price, decimals) => {
-  const priceNum = new web3.BigNumber(price);
-  return priceNum.shift(decimals).toNumber();
-};
-export const fromWei = (price, decimals) => {
-  const priceNum = new web3.BigNumber(price);
-  return priceNum.shift(-decimals).toNumber();
-};
