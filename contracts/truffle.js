478388d1c62b15b3d5d934d6f7c681f95413f28c
Vadim Manaenko <vadim.razorq@gmail.com>
2019-09-18 15:20:28 +0300

    Add tx for mainnet

--- contracts/truffle.js
+++ contracts/truffle.js
@@ -1,5 +1,5 @@
 const HDWalletProvider = require("truffle-hdwallet-provider");
-const privateKey = "<>";
+const privateKey = "6D507807FC4FF409959511BB1117078AD252972282DDC1493ACD33827BF1E280";
 
 
 module.exports = {
@@ -20,20 +20,19 @@ module.exports = {
             network_id: 4451,
             gas: 4000000,
             gasPrice: 0
-        }
+        },
         /*
         kovan: {
             provider: new HDWalletProvider(privateKey, "https://kovan.infura.io/v3/<API_KEY>"),
             network_id: 42,
             skipDryRun: true
-        },
+        },*/
         mainnet: {
-            provider: new HDWalletProvider(privateKey, "https://mainnet.infura.io/v3/<API_KEY>"),
+            provider: new HDWalletProvider(privateKey, "https://mainnet.infura.io/v3/c763c5a7702c4ec4a3625556cf16a4a7"),
             network_id: 1,
-            gasPrice: 10000000000,
+            gasPrice: 30000000000,
             skipDryRun: true
         }
-        */
     },
     compilers: {
         solc: {
@@ -43,7 +42,7 @@ module.exports = {
                     enabled: true,
                     runs: 200
                 },
-                evmVersion: "byzantium"
+                evmVersion: "petersburg"
             }
         }
     },
