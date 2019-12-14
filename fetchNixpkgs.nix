079c27e07a9c60a1024a016094676b204dbbaeed
Vadim Manaenko <vadim.razorq@gmail.com>
2019-12-14 15:20:28 +0300

    Update nixpkgs

--- fetchNixpkgs.nix
+++ fetchNixpkgs.nix
@@ -1,5 +1,5 @@
-{ rev    ? "74eac314cc1cb815fb42f4316e6a29b0d9f9a8a3"             # The Git revision of nixpkgs to fetch
-, sha256 ? "074ll7xhpl8h6pqsjl3src4822sccgnjz6nf9mg6k1h0lq06yrxc" # The SHA256 of the downloaded data
+{ rev    ? "5a25280e914de33e5dc3fb4e10b962dc571cfdf6"             # The Git revision of nixpkgs to fetch
+, sha256 ? "0iasfh1jqw99zbzqsrcg7fj02gsk46nmama4gkp3jn85gndhrs00" # The SHA256 of the downloaded data
 }:
 
 builtins.fetchTarball {
